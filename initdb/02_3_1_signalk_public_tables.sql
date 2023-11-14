ee131e0e70998a598e74f86e2f43b2fdcefb8b5a
xbgmsharp <xbgmsharp@gmail.com>
2023-11-15 00:16:25 +0100

    Update metrics_trigger_fn, fix stay postprocessing, only when gone.

--- initdb/02_3_1_signalk_public_tables.sql
+++ initdb/02_3_1_signalk_public_tables.sql
@@ -90,7 +90,7 @@ INSERT INTO public.email_templates VALUES
     E'Congratulations!\nYou have just connect your account to Pushover.\n'),
 ('email_otp',
     'Email verification',
-    E'Hello,\nPlease active your account using the following code: __OTP_CODE__.\nThe code is valid 15 minutes.\nThe PostgSail Team',
+    E'Hello,\nPlease active your account using the following code: __OTP_CODE__.\nThe code is valid 15 minutes.\nSee more details at __APP_URL__/activate\nThe PostgSail Team',
     'Email verification',
     E'Congratulations!\nPlease validate your account. Check your email!'),
 ('email_valid',
